32aa4ea0b9a5fd7d7f40aeda7ef47f0f21df1b71
Mafit <mafit.tifam@gmail.com>
2024-04-15 12:42:52 +0300

    Initial commit

---
+++ app/src/main/java/com/example/pr8/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.pr8;
+
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.lifecycle.Observer;
+import androidx.work.OneTimeWorkRequest;
+import androidx.work.WorkInfo;
+import androidx.work.WorkManager;
+
+import com.squareup.picasso.Picasso;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        ImageView imageView = new ImageView(MainActivity.this);
+
+        OneTimeWorkRequest workRequest = new OneTimeWorkRequest.Builder(com.example.pr8.MyWorker.class).build();
+        WorkManager.getInstance(this).enqueue(workRequest);
+
+        WorkManager.getInstance(this).
+                getWorkInfoByIdLiveData(workRequest.getId()).observe(this, new Observer<WorkInfo>(){
+                        @Override
+                        public void onChanged(WorkInfo workInfo) {
+                            String url = workInfo.getOutputData().getString("key1");
+                            Picasso.get().load(url).into(imageView);
+                        }
+                    }
+                );
+        setContentView(imageView);
+    }
+}
